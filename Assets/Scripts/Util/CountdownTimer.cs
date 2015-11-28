1561cfd64a1246ceda337c278c8ebd398fbbe24f
Eric Cook <jcook83@gatech.edu>
2015-11-27 22:03:49 -0500

    Countdown timer
    Most likely will be used for game logic

---
+++ Assets/Scripts/Util/CountdownTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Util
+{
+    public class CountdownTimer : Timer
+    {
+        // Delegates and events to fire once timer times out
+        new public delegate void TimerEvent(CountdownTimer t);
+        new public event TimerEvent TimeOut;
+
+        public override void Initialize(float interval, string id)
+        {
+            base.Initialize(interval, id);
+            timer = interval;
+        }
+
+        protected override void UpdateTimer()
+        {
+            if (on)
+            {
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    timer = 0;
+                    // If the event has subscribers, fire it
+                    if (TimeOut != null) TimeOut(this);
+                    Destroy(this);
+                }
+            }
+        }
+    }
+}
