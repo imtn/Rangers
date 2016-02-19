d9b7d2ce5f759dcdcc2195aafe64ab22db1cbfb6
taylor1577 <hearntaylor@gmail.com>
2016-02-19 11:16:39 -0500

    Lifesteal Arrow Type
    Adds a new type of arrow, which heals the player who shot it when it
    hits another player.

---
+++ Assets/Scripts/Arrows/LifestealArrow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Arrows
+{
+	/// <summary>
+	/// Arrow property that gives life to the player who shot it when it hits another player.
+	/// </summary>
+	public class LifestealArrow : ArrowProperty
+	{
+
+		public override void Effect(PlayerID hitPlayer)
+		{
+			// If a player was hit
+			if (hitPlayer != 0)
+			{
+				Player.Controller sourceController = Data.GameManager.instance.GetPlayer(fromPlayer);
+				float damage = GetComponent<ArrowController>().GetDamage();
+				sourceController.LifeComponent.ModifyHealth(damage);
+			}
+		}
+	}
+}
+
