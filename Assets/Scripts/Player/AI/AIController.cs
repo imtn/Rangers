95b2ec50645689a15307aa580ff0fd8f72dc77ba
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2016-02-19 12:57:29 -0500

    Very basic "stand and shoot" AI
    -Stands still and shoots medium-power arrows at the opponent

---
+++ Assets/Scripts/Player/AI/AIController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Data;
+
+namespace Assets.Scripts.Player.AI
+{
+	/// <summary> Interface for AI to control a character. </summary>
+	public class AIController : Controller
+    {
+		private IPolicy policy;
+
+		/// <summary> The speed that the character is moving at. </summary>
+		[SerializeField]
+		public float runSpeed = 0;
+
+		/// <summary> Whether the character is jumping. </summary>
+		[SerializeField]
+		public bool jump = false;
+		/// <summary> Whether the character is sliding. </summary>
+		[SerializeField]
+		public bool slide = false;
+
+		/// <summary> Whether the character is aiming a shot. </summary>
+		[SerializeField]
+		public bool aiming = false;
+		/// <summary> Whether the character was aiming its shot on the last tick. </summary>
+		private bool wasAiming = false;
+		/// <summary> Vector in the direction that the character is aiming. </summary>
+		[SerializeField]
+		public Vector3 aim = Vector3.zero;
+
+		/// <summary> The opponent of this character. </summary>
+		public Controller opponent;
+
+		/// <summary> Initializes the AI policy to use. </summary>
+		void Start()
+		{
+			policy = new StandShoot();
+			foreach (Controller controller in GameManager.instance.AllPlayers)
+			{
+				if (controller != this)
+				{
+					opponent = controller;
+					break;
+				}
+			}
+		}
+		
+		/// <summary> Moves the agent every tick. </summary>
+		void Update()
+		{
+			policy.ChooseAction(this);
+
+			if (jump)
+			{
+				parkour.Jump();
+			}
+
+			if (slide)
+			{
+				parkour.SlideOn();
+			}
+			else
+			{
+				parkour.SlideOff();
+			}
+
+			if (aiming)
+			{
+				archery.UpdateFirePoint(Vector3.Normalize(aim));
+				wasAiming = true;
+			}
+			else if (wasAiming)
+			{
+				wasAiming = false;
+				archery.Fire();
+			}
+			else
+			{
+				archery.AimUpperBodyWithLegs();
+			}
+		}
+
+		/// <summary> Updates the player's running movement. </summary>
+		void FixedUpdate()
+		{
+			if (life.Health > 0)
+			{
+				parkour.Locomote(runSpeed);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player/AI/StandShoot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Player.AI
+{
+	/// <summary> AI that always stands still and shoots at the player. </summary>
+	public class StandShoot : IPolicy
+	{
+		/// <summary> Timer for delay between firing. </summary>
+		private float fireCooldown = 0;
+		/// <summary> The delay between firing. </summary>
+		private const float COOLDOWNTIME = 5;
+
+		public void ChooseAction(AIController controller)
+		{
+			if (controller.aiming)
+			{
+				if (controller.ArcheryComponent.StrengthPercentage > 0.5)
+				{
+					controller.aiming = false;
+				}
+			}
+			else
+			{
+				if (fireCooldown++ > COOLDOWNTIME)
+				{
+					controller.aim = Vector3.Normalize(controller.opponent.transform.position - controller.transform.position);
+					Debug.Log(controller.aim);
+					controller.aiming = true;
+					fireCooldown = 0;
+				}
+			}
+		}
+	}
+}
+
