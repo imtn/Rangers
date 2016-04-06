2cdba4a903baf174fad13d047ca3c186c3ce43f4
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2016-04-06 14:11:43 -0400

    Tabs...

--- Assets/Scripts/Util/Enums.cs
+++ Assets/Scripts/Util/Enums.cs
@@ -7,14 +7,14 @@
     {
         public enum Arrows { Normal, Fireball, Ice, Thunder, Acid, Ricochet, Ghost, ZeroGravity, Tracking, Lifesteal, Virus, Splitting, HeavyKnockback, RapidFire, Grappling, NumTypes }
         public enum Tokens { Fireball, Ice, Thunder, Acid, Ricochet, Ghost, ZeroGravity, Tracking, Lifesteal, Virus, Splitting, HeavyKnockback, RapidFire, Grappling, Health, NumTypes }
-		public enum GameType { Deathmatch, Stock, NumTypes }
-		public enum GameVariant { None, TokensForEveryone, NumTypes }
+        public enum GameType { Deathmatch, Stock, NumTypes }
+        public enum GameVariant { None, TokensForEveryone, NumTypes }
         public enum RepetitionTimerSettings { Limited, Unlimited }
         public enum Frequency { None, Sparce, Infrequent, Average, Frequent, Abundant, NumTypes }        
         public enum MenuDirections { Up, Down, Left, Right}
         public enum UIStates { Splash, Main, SinglePlayer, Multiplayer, Settings, Audio, Video, Signin, ArenaStandard, LevelSelect, ValueModifier, TargetLevelSelect, None}
 
-		public enum BattleStages { ProLeagueStandard, NumStages }
-		public enum TargetPracticeStages { Intro, MagneticDistortion, NumStages }
+        public enum BattleStages { ProLeagueStandard, NumStages }
+        public enum TargetPracticeStages { Intro, MagneticDistortion, NumStages }
     }
 }
